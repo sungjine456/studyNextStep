02ecb5d4046e21c52c7c4a4b277a8868b7151b56
sungjine456 <sungjine456@gmail.com>
2017-01-14 15:00:28 +0900

    3.4.3.1 요구사항 1 - index.html 응답하기

--- src/main/java/org/studyStepNext/part3/webserver/RequestHandler.java
+++ src/main/java/org/studyStepNext/part3/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package org.studyStepNext.part3.webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,10 +26,17 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream(); BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+        	String line = br.readLine();
+        	String[] token = line.split(" ");
+        	while(!"".equals(line)){
+        		if(line == null){
+        			break;
+        		}
+        		line = br.readLine();
+        	}
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./WebContent" + token[1]).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {

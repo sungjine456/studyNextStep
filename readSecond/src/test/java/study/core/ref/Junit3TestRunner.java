1918c9d63fae0ab1c601af0c1b49b730de11655c
sungjine456 <sungjine456@gmail.com>
2017-06-25 16:56:09 +0900

    reflection 연습

--- readSecond/src/test/java/study/core/ref/Junit3TestRunner.java
+++ readSecond/src/test/java/study/core/ref/Junit3TestRunner.java
@@ -1,11 +1,17 @@
 package study.core.ref;
 
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
+        for(Method method : clazz.getMethods()){
+        	if(method.getName().contains("test")){
+        		method.invoke(clazz.newInstance());
+        	}
+        }
     }
 }
--- readSecond/src/test/java/study/core/ref/ReflectionTest.java
+++ readSecond/src/test/java/study/core/ref/ReflectionTest.java
@@ -1,5 +1,12 @@
 package study.core.ref;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,17 +21,41 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+        for(Field field : clazz.getDeclaredFields()){
+        	logger.info(field.getName());
+        }
+        for(Constructor<?> con : clazz.getDeclaredConstructors()){
+        	logger.info(con.getName() + " " + con.getParameterCount());
+        }
+        for(Method method : clazz.getDeclaredMethods()){
+        	logger.info(method.getName());
+        }
     }
     
     @Test
-    public void newInstanceWithConstructorArgs() {
+    public void newInstanceWithConstructorArgs() throws Exception {
         Class<User> clazz = User.class;
         logger.debug(clazz.getName());
+        Constructor<User> con = clazz.getConstructor(String.class,String.class,String.class,String.class);
+        User user = con.newInstance("id","password","name","email");
+        assertThat(user.getName(), is("name"));
+        assertThat(user.getEmail(), is("email"));
+        assertThat(user.getPassword(), is("password"));
+        assertThat(user.getUserId(), is("id"));
     }
     
     @Test
-    public void privateFieldAccess() {
+    public void privateFieldAccess() throws Exception {
         Class<Student> clazz = Student.class;
         logger.debug(clazz.getName());
+        Student student = clazz.newInstance();
+        Field age = clazz.getDeclaredField("age");
+        Field name = clazz.getDeclaredField("name");
+        age.setAccessible(true);
+        name.setAccessible(true);
+        age.setInt(student, 10);
+        name.set(student, "jon");
+        assertThat(student.getAge(), is(10));
+        assertThat(student.getName(), is("jon"));
     }
 }
